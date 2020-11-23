37f6d76685b034c8cda2c2d5c9e28b00ae19e180
snaplib <snap@snaplib.org>
2020-11-23 00:27:24 -0800

    Add vairous contains methods

--- src/main/ts/Directory.ts
+++ src/main/ts/Directory.ts
@@ -24,14 +24,60 @@ export class Directory extends AbstractElement
         return this.getContentNames().map<string>(dirContentName => pathModule.join(this.path, dirContentName));
     }
 
-    public getDirNames(): ReadonlyArray<string>
+    public getDirPaths(): ReadonlyArray<string>
     {
         return this.getContentPaths().filter(dirContentPath => fs.lstatSync(dirContentPath).isDirectory());
     }
 
-    public getFileNames(): ReadonlyArray<string>
+    public getDirNames(): ReadonlyArray<string>
+    {
+        return this.getDirPaths().map(dirPath => pathModule.basename(dirPath));
+    }
+
+    public getFilePaths(): ReadonlyArray<string>
     {
         return this.getContentPaths().filter(dirContentPath => fs.lstatSync(dirContentPath).isFile());
+    }
+
+    public getFileNames(): ReadonlyArray<string>
+    {
+        return this.getFilePaths().map(filePath => pathModule.basename(filePath));
+    }
+
+    public contains(nameOrPath: string): boolean
+    {
+        return this.getContentNames().includes(nameOrPath)
+            || this.getContentPaths().includes(nameOrPath)
+    }
+
+    public containsIgnoreCase(nameOrPath: string): boolean
+    {
+        return this.getContentNames().some(contentName => nameOrPath.localeCompare(contentName))
+            || this.getContentPaths().some(contentPath => nameOrPath.localeCompare(contentPath))
+    }
+
+    public containsFile(fileNameOrPath: string): boolean
+    {
+        return this.getFileNames().includes(fileNameOrPath)
+            || this.getFilePaths().includes(fileNameOrPath)
+    }
+
+    public containsFileIgnoreCase(nameOrPath: string): boolean
+    {
+        return this.getFileNames().some(fileName => nameOrPath.localeCompare(fileName))
+            || this.getFilePaths().some(filePath => nameOrPath.localeCompare(filePath))
+    }
+
+    public containsDir(dirNameOrPath: string): boolean
+    {
+        return this.getDirNames().includes(dirNameOrPath)
+            || this.getDirPaths().includes(dirNameOrPath)
+    }
+
+    public containsDirIgnoreCase(nameOrPath: string): boolean
+    {
+        return this.getDirNames().some(dirName => nameOrPath.localeCompare(dirName))
+            || this.getDirPaths().some(dirPath => nameOrPath.localeCompare(dirPath))
     }
 
     public size(): number
