c946b3dedb232d84ba73b6eba7c10ea56b5bf75c
snaplib <snap@snaplib.org>
2020-11-27 21:42:40 -0800

    Remove tests

--- src/test/ts/TestAbstractElement.ts
+++ src/test/ts/TestAbstractElement.ts
@@ -4,16 +4,7 @@ import 'mocha';
 
 const CWD: string = process.cwd();
 
-const MOCK_ABSTRACT_ELEMENT: AbstractElement = new AbstractElement(CWD);
-
 describe('new AbstractElement("path/to/existing/directory") valid constructors', () => {
-
-    context('no element status properties properly set', () => {
-        it(`should not throw exception with new AbstractElement("${CWD}")`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD),
-                `new AbstractElement("${CWD}") threw exception`)
-        });
-    });
 
     context('single element status properties properly set', () => {
         it(`should not throw exception with new AbstractElement("${CWD}", {exists: true})`, () => {
@@ -31,40 +22,4 @@ describe('new AbstractElement("path/to/existing/directory") valid constructors',
                 `new AbstractElement("${CWD}", {isFile: false}) threw exception`)
         });
     });
-
-    context('double element status properties properly set', () => {
-        it(`should not throw exception with new AbstractElement("${CWD}", {exists: true, isDirectory: true})`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD, {exists: true, isDirectory: true}),
-                `new AbstractElement("${CWD}", {exists: true, isDirectory: true}) threw exception`)
-        });
-
-        it(`should not throw exception with new AbstractElement("${CWD}", {exists: true, isFile: false})`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD, {exists: true, isFile: false}),
-                `new AbstractElement("${CWD}", {exists: true, isDirectory: true}) threw exception`)
-        });
-
-        it(`should not throw exception with new AbstractElement("${CWD}", {isDirectory: true, isFile: false})`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD, {isDirectory: true, isFile: false}),
-                `new AbstractElement("${CWD}", {isDirectory: true, isFile: false}) threw exception`)
-        });
-    });
-
-    context('triple element status properties properly set', () => {
-        it(`should not throw exception with new AbstractElement("${CWD}", {exists: true, isDirectory: true, isFile: false})`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD, {exists: true, isDirectory: true, isFile: false}),
-                `new AbstractElement("${CWD}", {exists: true, isDirectory: true, isFile: false}) threw exception`)
-        });
-    });
 });
-
-describe("unimplemented getContents() and size()", () => {
-    it('should throw MissingMethodImplementationError', () => {
-        assert.throws(() => MOCK_ABSTRACT_ELEMENT.getContents(),
-            "missing getContents() method implementation")
-    });
-
-    it('should throw MissingMethodImplementationError', () => {
-        assert.throws(() => MOCK_ABSTRACT_ELEMENT.size(),
-            "missing size() method implementation")
-    });
-})
