3ade89babca58b4c10ff69a044a4fec899bdc8bf
snaplib <snap@snaplib.org>
2020-11-28 20:26:52 -0800

    Remove all tests pending rewrite of ElementStatus.ts

--- src/test/ts/TestAbstractElement.ts
+++ src/test/ts/TestAbstractElement.ts
@@ -6,20 +6,7 @@ const CWD: string = process.cwd();
 
 describe('new AbstractElement("path/to/existing/directory") valid constructors', () => {
 
-    context('single element status properties properly set', () => {
-        it(`should not throw exception with new AbstractElement("${CWD}", {exists: true})`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD, {exists: true}),
-                `new AbstractElement("${CWD}", {exists: true}) threw exception`)
-        });
+    context("", () => {
 
-        it(`should not throw exception with new AbstractElement("${CWD}", {isDirectory: true})`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD, {isDirectory: true}),
-                `new AbstractElement("${CWD}", {isDirectory: true}) threw exception`)
-        });
-
-        it(`should not throw exception with new AbstractElement("${CWD}", {isFile: false})`, () => {
-            assert.doesNotThrow(() => new AbstractElement(CWD, {isFile: false}),
-                `new AbstractElement("${CWD}", {isFile: false}) threw exception`)
-        });
     });
 });
