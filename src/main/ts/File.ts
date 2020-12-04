66f4eb691fcf8d8c77d217f0df3f682e40e33e1f
snaplib <snap@snaplib.org>
2020-12-03 22:55:56 -0800

    Update so typescript src can be transpiled

--- src/main/ts/File.ts
+++ src/main/ts/File.ts
@@ -1,6 +1,20 @@
-import { Element  } from "./Element";
+import { Element } from "./Element";
+import * as fs from "fs";
 
 export class File extends Element
 {
+    public constructor(filePath: string, existsStatus: boolean)
+    {
+        super(filePath, {exists: existsStatus, isDirectory: false, isFile: true});
+    }
 
+    contents(): ReadonlyArray<string>
+    {
+        return undefined;
+    }
+
+    size(): number
+    {
+        return this.exists() ? fs.statSync(this.elementPath).size : -1;
+    }
 }
