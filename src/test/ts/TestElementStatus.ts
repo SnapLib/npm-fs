a230155f172f38a79fa650fe41cabeab77ed8126
snaplib <snap@snaplib.org>
2020-12-03 22:35:42 -0800

    Switch anonomys functions to arrow functions

--- src/test/ts/TestElementStatus.ts
+++ src/test/ts/TestElementStatus.ts
@@ -1126,14 +1126,14 @@ describe("invalid static of(Partial<ElementStatus>) instantiation", () => {
 });
 
 describe("ElementStatus.ofExistingDirectory()", () => {
-    it('should equal new ElementStatus(true, true, false)', function () {
+    it('should equal new ElementStatus(true, true, false)', () => {
         assert.deepStrictEqual(ElementStatus.ofExistingDirectory(), new ElementStatus(true, true, false),
             "ElementStatus.ofExistingDirectory() did not equal new ElementStatus(true, true, false)");
     });
 });
 
 describe("ElementStatus.ofExistingFile()", () => {
-    it('should equal new ElementStatus(true, false, true)', function () {
+    it('should equal new ElementStatus(true, false, true)', () => {
         assert.deepStrictEqual(ElementStatus.ofExistingFile(), new ElementStatus(true, false, true),
             "ElementStatus.ofExistingFile() did not equal new ElementStatus(true, false, true)");
     });
