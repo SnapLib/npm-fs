bfea4ee9551c83960b81b5a7b16b13b4671687a4
Snap <XxSnapperGeexX@gmail.com>
2020-12-07 21:01:29 -0800

    Add doc comments

--- src/main/ts/structure/Structure.ts
+++ src/main/ts/structure/Structure.ts
@@ -9,9 +9,35 @@
  */
 export class Structure
 {
+    /**
+     * The directories that should be present in this directory structure
+     *
+     * @readonly
+     * @property
+     */
     public readonly directories: ReadonlyArray<string>;
+
+    /**
+     * The files that should be present in this directory structure
+     *
+     * @readonly
+     * @property
+     */
     public readonly files: ReadonlyArray<string>;
 
+    /**
+     * Constructor that takes in a string array of directory names and file
+     * names that should be present in this directory structure.
+     *
+     * @param directories array of directory names that should be present in
+     *                    this directory structure
+     *
+     * @param files array of file names that should be present in this directory
+     *              structure
+     *
+     *  @package
+     *  @constructor
+     */
     constructor(directories: ReadonlyArray<string>, files: ReadonlyArray<string>)
     {
         this.directories = directories;
