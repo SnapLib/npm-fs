01ec69cc93ae18cad7d408364312d31970f0e031
Snap <XxSnapperGeexX@gmail.com>
2020-12-13 18:03:10 -0800

    Remove accessors

--- src/main/ts/element/File.ts
+++ src/main/ts/element/File.ts
@@ -3,35 +3,32 @@ import * as fs from "fs";
 
 export class File extends AbstractElement
 {
-    private readonly _fileString: string;
-    private readonly _lines: ReadonlyArray<string>
+    public readonly fileString: string;
+    public readonly lines: ReadonlyArray<string>;
+    public readonly length: number;
 
     public constructor(filePath: string, existsStatus: boolean)
     {
         super(filePath, {exists: existsStatus, isDirectory: false, isFile: true});
 
-        this._fileString = fs.readFileSync(this.elementPath, {encoding: "utf8"});
-        this._lines = this._fileString.split("\n");
+        this.fileString = fs.readFileSync(this.elementPath, {encoding: "utf8"});
+        this.lines = this.fileString.split("\n");
+        this.length = this.exists() ? this.lines.length : -1;
     }
 
     public contents(): ReadonlyArray<string>
     {
-        return this._lines;
+        return this.lines;
     }
 
     public contains(aString: string): boolean
     {
-        return this._fileString.includes(aString);
+        return this.fileString.includes(aString);
     }
 
     public isEmpty(): boolean
     {
-        return this._fileString.length === 0;
-    }
-
-    public get length(): number
-    {
-        return this.exists() ? this._lines.length : -1;
+        return this.fileString.length === 0;
     }
 
     public size(): number
