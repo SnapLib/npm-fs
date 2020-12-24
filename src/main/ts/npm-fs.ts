2f8dfeaf26b7d686a7f290fbef59b6e707018e68
Snap <XxSnapperGeexX@gmail.com>
2020-12-23 22:45:01 -0800

    Begin using new default required/optional node and snplib proj values

--- src/main/ts/npm-fs.ts
+++ src/main/ts/npm-fs.ts
@@ -1,69 +1,94 @@
-import { dirname } from "path";
-import {Root} from "./environment/Root";
+import {dirname} from "path";
+import {RootDirectory} from "./environment/root/RootDirectory";
+import {JSONFile} from "./element/JSONFile";
+import {RootDirStructure} from "./environment/structure/RootDirStructure";
+// import * as fs from "fs";
 
 /**
- * This is the entry point of the SnapLib `npm-fs` node package.
+ * This is the entry point of the SnapLib `npm-fs` package.
  *
  * @classdesc
- * This class is responsible for consuming and parsing command line arguments as
- * well as acts as the origin point for validating and parsing the npm package
- * structure this package is installed to as a dependency.
+ * This class is responsible for consuming command line arguments as well as
+ * acts as the origin point for validating and parsing the npm package structure
+ * this package is installed to as a dependency.
  *
  * @remarks
- * It is assumed that this package will be used in npm based projects wishing to
- * adhere to the SnapLib npm project structure. This API breaks a SnapLib
- * categorizes project structures. The first simply being an npm project, the
- * second being a SnapLib npm project.
- *
- * @remarks
- * Building on top of the above remark, it can be assumed that this package
- * directory will be located in the root `node_modules` directory and that this
- * source file will be located in the root of this package directory. So if a programmer has
- * a project with the root project directory "my-npm-project", it'd be
- * structured like:
- *
- * @example Sample SnapLib npm project structure
- * ```shell
- * .
- * ├──src
- * ├──node_modules
- * │  └──npm-fs
- * │     └── npm-fs.js
- * ```
+ * It is assumed that this package directory will be located in the root
+ * `node_modules` directory and that this source file will be located in the
+ * root of this package directory.
  *
  * @author Snap <XxSnapperGeexX@gmail.com>
  */
 export class NpmFS
 {
-    private readonly rootDir: Root;
+    /**
+     * @private
+     * @readonly
+     * @property
+     */
+    private readonly _cliArgs: ReadonlyArray<string>;
 
-    private constructor(private readonly cliArgs: ReadonlyArray<string>)
+    /**
+     * @private
+     * @readonly
+     * @property
+     */
+    private readonly _rootDir: RootDirectory;
+
+    /** @constructor */
+    private constructor(cliArgs: ReadonlyArray<string>)
     {
-        if (dirname(__dirname) === "node-modules")
+        this._cliArgs = cliArgs;
+
+        if (this._cliArgs.length === 0)
         {
-            throw new Error("Invalid node modules directory");
+            console.log("no cli args passed");
+        }
+        else
+        {
+            console.log(`cli args passed: ["${this._cliArgs.join('", "')}"]`);
+        }
+
+        if (dirname(__dirname) !== "node_modules")
+        {
+            console.log("entry point not in node modules");
         }
 
         // Set root directory to parent directory that contains "node_modules"
         // directory
-        this.rootDir = new Root((dirname(dirname(__dirname))));
+        this._rootDir = new RootDirectory((dirname(dirname(__dirname))));
 
-        // S
         const missingNPMDirectories: ReadonlyArray<string> =
-            Root.REQUIRED_NODE.directories.filter(requiredNodeDir => ! this.rootDir.containsDirIgnoreCase(requiredNodeDir));
+            RootDirStructure.required.node.directories.filter(requiredNodeDir => ! this._rootDir.containsDirIgnoreCase(requiredNodeDir));
+            // RootDirectory.REQUIRED_NODE.directories.filter(requiredNodeDir => ! this._rootDir.containsDirIgnoreCase(requiredNodeDir));
 
         const missingNPMFiles: ReadonlyArray<string> =
-            Root.REQUIRED_NODE.files.filter(requiredNodeFile => ! this.rootDir.containsFileIgnoreCase(requiredNodeFile));
+            RootDirectory.REQUIRED_NODE.files.filter(requiredNodeFile => ! this._rootDir.containsFileIgnoreCase(requiredNodeFile));
 
-        console.log(this.rootDir);
+        const missingSnapDirectories: ReadonlyArray<string> =
+            RootDirectory.DEFAULT_REQUIRED.directories.filter(requiredSnapDir => ! this._rootDir.containsDirIgnoreCase(requiredSnapDir));
+
+        const missingSnapFiles: ReadonlyArray<string> =
+            RootDirectory.DEFAULT_REQUIRED.files.filter(requiredSnapFile => ! this._rootDir.containsFileIgnoreCase(requiredSnapFile));
+
+        const jsonFile: JSONFile = new JSONFile(this._rootDir.path.concat("/package.json"));
+
+        console.log(`Package.json entries: ${jsonFile.entries()}\n`);
+        console.log(`Package.json keys: ${jsonFile.keys()}\n`);
+        console.log(`Package.json values: ${jsonFile.values()}\n`);
+        console.log(`Package.json obj:\n${jsonFile.toString()}\n`);
+
         console.log(`Missing node dirs: ["${missingNPMDirectories.join('", "')}"]`);
         console.log(`Missing node files: ["${missingNPMFiles.join('", "')}"]`);
+        console.log(`Missing SnapLib dirs: ["${missingSnapDirectories.join('", "')}"]`);
+        console.log(`Missing SnapLib files: ["${missingSnapFiles.join('", "')}"]`);
 
         // if (missingNPMDirectories.length !== 0)
         // {
         //     throw new Error(`missing required npm directories: ${missingNPMDirectories}`);
         // }
-        // else if (missingNPMFiles.length !== 0)
+
+        // if (missingNPMFiles.length !== 0)
         // {
         //     throw new Error(`missing required npm files: ${missingNPMFiles}`);
         // }
@@ -72,6 +97,12 @@ export class NpmFS
     public static exec(cliArgs?: ReadonlyArray<string>): void
     {
         const npmFs: NpmFS = new NpmFS(cliArgs ?? process.argv.slice(2));
+
+        // To get rid of TS lint error
+        if (npmFs)
+        {
+            true;
+        }
     }
 }
 
