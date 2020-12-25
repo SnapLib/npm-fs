d0433c1a71e7511ed3f82369b1f51a195101aef4
Snap <XxSnapperGeexX@gmail.com>
2020-12-25 00:18:32 -0800

    Remove references to old DirContents class

--- src/main/ts/environment/root/RootDirectory.ts
+++ src/main/ts/environment/root/RootDirectory.ts
@@ -1,49 +1,8 @@
-import {DirContents} from "../DirContents";
-import * as fs from "fs";
 import {Directory} from "../../element/Directory";
+import * as fs from "fs";
 
 export class RootDirectory extends Directory
 {
-    /**
-     * Files and directories that should be present in all npm project root
-     * directories.
-     *
-     * @remarks
-     * If these aren't present, indicates that an npm project root directory is
-     * malformed or the root directory isn't a root directory of an npm project.
-     *
-     * @static
-     * @readonly
-     */
-    public static readonly REQUIRED_NODE: Readonly<DirContents> =
-        new DirContents(["node_modules"], ["package.json", "package-lock.json"]);
-
-    /**
-     * Files and directories that should be present in all SnapLib based npm
-     * projects.
-     *
-     * @remarks
-     * These differ from the {@link REQUIRED_NODE} directories and files in that
-     * these are specific to SnapLib based npm projects. If these aren't
-     * present, indicates that this npm project is not following SnapLib project
-     * specifications.
-     *
-     * @static
-     * @readonly
-     */
-    public static readonly DEFAULT_REQUIRED: Readonly<DirContents> =
-        new DirContents(["src"], [".editorconfig", ".eslintrc.json", "typedoc.json"]);
-
-    /**
-     * Default optional root directory file names and directory names.
-     *
-     * @static
-     * @readonly
-     */
-    public static readonly DEFAULT_OPTIONAL: Readonly<DirContents> =
-        new DirContents(["docs", ".git"],
-                        [".gitignore", "LICENSE.txt", "README.md"]);
-
     public constructor(rootPath: string)
     {
         if ( ! fs.existsSync(rootPath))
