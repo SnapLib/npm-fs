dc2c8633396713eaa71529fe046bdb7325b35bc5
Snap <XxSnapperGeexX@gmail.com>
2020-12-25 19:06:51 -0800

    Add properties and telescoping setters for required files and directories

--- src/main/ts/environment/root/RootDirectory.ts
+++ src/main/ts/environment/root/RootDirectory.ts
@@ -1,8 +1,12 @@
 import {Directory} from "../../element/Directory";
+import { DirContents } from "../structure/DirContents";
 import * as fs from "fs";
 
 export class RootDirectory extends Directory
 {
+    public required: DirContents;
+
+    public optional: DirContents;
     public constructor(rootPath: string)
     {
         if ( ! fs.existsSync(rootPath))
@@ -19,7 +23,35 @@ export class RootDirectory extends Directory
         }
         else
         {
+            const emptyArray: ReadonlyArray<string> = [];
+
             super(rootPath);
+            this.required = new DirContents(emptyArray,emptyArray);
+            this.optional = new DirContents(emptyArray,emptyArray);
         }
+    }
+
+    public addRequiredDirs(...dirNames: ReadonlyArray<string>): RootDirectory
+    {
+        this.required = new DirContents(this.required.directories.concat(dirNames), this.required.files);
+        return this;
+    }
+
+    public addRequiredFiles(...fileNames: ReadonlyArray<string>): RootDirectory
+    {
+        this.required = new DirContents(this.required.directories, this.required.files.concat(fileNames));
+        return this;
+    }
+
+    public addOptionalDirs(...dirNames: ReadonlyArray<string>): RootDirectory
+    {
+        this.optional = new DirContents(this.optional.directories.concat(dirNames), this.optional.files);
+        return this;
+    }
+
+    public addOptionalFiles(...fileNames: ReadonlyArray<string>): RootDirectory
+    {
+        this.optional = new DirContents(this.optional.directories, this.optional.files.concat(fileNames));
+        return this;
     }
 }
