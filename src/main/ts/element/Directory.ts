b3d76fd1573a02323ea7e72684e935407527e494
Snap <XxSnapperGeexX@gmail.com>
2021-01-03 15:20:39 -0800

    Add function signatures

--- src/main/ts/element/Directory.ts
+++ src/main/ts/element/Directory.ts
@@ -1,8 +1,48 @@
 import type {Element} from "./Element";
+import type {File} from "./File";
+import type {Dirent} from "fs";
 
-interface Directory extends Element
+export interface Directory extends Element
 {
     readonly inode: number;
 
+    /**
+     * Returns the directory entries of this directory element.
+     *
+     * @returns the directory entries of this directory element.
+     *
+     * @abstract
+     * @function
+     */
+    direntsSync(): ReadonlyArray<Dirent>;
+
+    fileElementsSync(): ReadonlyArray<File>;
+
+    dirElementsSync(): ReadonlyArray<Directory>;
+
     contentsSync(): ReadonlyArray<Element>;
+
+    fileNamesSync(): ReadonlyArray<string>;
+
+    dirNamesSync(): ReadonlyArray<string>;
+
+    direntNamesSync(): ReadonlyArray<string>;
+
+    filePathsSync(): ReadonlyArray<string>;
+
+    dirPathsSync(): ReadonlyArray<string>;
+
+    direntPathsSync(): ReadonlyArray<string>;
+
+    relativeFilePathsSync(): ReadonlyArray<string>;
+
+    relativeDirPathsSync(): ReadonlyArray<string>;
+
+    relativeDirentPathsSync(): ReadonlyArray<string>;
+
+    containsFileSync(fileName: string, options?: {caseSensitive: boolean, recursive: boolean}): boolean;
+
+    containsDirSync(dirName: string, options?: {caseSensitive: boolean, recursive: boolean}): boolean;
+
+    containsSync(fileOrDirName: string, options?: {caseSensitive: boolean, recursive: boolean}): boolean;
 }
