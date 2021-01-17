67b302eab7244dafc478d71130a64f67d0e65da3
Snap <XxSnapperGeexX@gmail.com>
2021-01-16 20:53:38 -0800

    Use direntSync(), fileSync(), and dirSync() methods to generate values based on readonly dirent array
    Improve toString() method

--- src/main/ts/lib/element/directory/VirtualDirElement.ts
+++ src/main/ts/lib/element/directory/VirtualDirElement.ts
@@ -1,24 +1,59 @@
 import AbstractDirElement from "./AbstractDirElement.js";
-import { normalize, sep } from "path";
+import type { VirtualDirents } from "./DirElement.js";
+import path from "path";
 
 export class VirtualDirElement extends AbstractDirElement
 {
-    private readonly _fileNames: Set<string> = new Set<string>();
+    readonly #relativeFilePaths: Set<string> = new Set<string>();
+    readonly #relativeDirPaths: Set<string> = new Set<string>();
 
-    private readonly _dirNames: Set<string> = new Set<string>();
-
-    public constructor(path:string, ...morePaths: ReadonlyArray<string>)
+    public constructor(virtualDirPath:string, ...morePaths: ReadonlyArray<string>)
     {
-        super(normalize([path].concat(morePaths).join(sep)), {exists: false});
+        super(path.normalize([virtualDirPath].concat(morePaths).join(path.sep)), {exists: false});
+    }
+
+    public fileSync(): VirtualDirents
+    {
+        const fileRelativePathsArray: ReadonlyArray<string> =
+            Array.from(this.#relativeFilePaths);
+
+        return {
+            names: fileRelativePathsArray.map(relFilePath => path.dirname(relFilePath)),
+            paths: fileRelativePathsArray.map(fileName => path.join(this.path, fileName))
+        };
+    }
+
+    public dirSync(): VirtualDirents
+    {
+        const dirNamesArray: ReadonlyArray<string> = Array.from(this.#relativeDirPaths);
+
+        return {
+            names: dirNamesArray,
+            paths: dirNamesArray.map(dirName => path.join(this.path, dirName))
+        };
+    }
+
+    public direntSync(): VirtualDirents
+    {
+        return {
+            names: this.fileSync().names.concat(this.dirSync().names),
+            paths: this.fileSync().paths.concat(this.dirSync().paths)
+        };
     }
 
     public fileNamesSync(): readonly string[]
     {
-        return Array.from(this._fileNames);
+        return this.fileSync().names;
     }
+
     public dirNamesSync(): readonly string[]
     {
-        return Array.from(this._dirNames);
+        return this.dirSync().names;
+    }
+
+    public toString(): string
+    {
+        return "VirtualDirElement:\n".concat(super.toString());
     }
 }
 
