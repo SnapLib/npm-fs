8f3d09539d7776d51236cf84f3ef7d09bf79868c
Snap <XxSnapperGeexX@gmail.com>
2021-01-16 20:55:30 -0800

    Add Dirents interface and ExistingDirents and VirtualDirents as alias type to Dirents interface
    Add fileSync(), dirSync(), and direntSync() methods with ExistingDirents | VirtualDirents return type

--- src/main/ts/lib/element/directory/DirElement.ts
+++ src/main/ts/lib/element/directory/DirElement.ts
@@ -1,7 +1,25 @@
 import type Element from "../Element.js";
+import type {Dirent} from "fs";
+
+export interface Dirents
+{
+        dirents?: ReturnType<() => ReadonlyArray<Dirent>>,
+        names: ReturnType<() => ReadonlyArray<string>>,
+        paths: ReturnType<() => ReadonlyArray<string>>
+}
+
+export type ExistingDirents = Required<Required<Dirents>>;
+
+export type VirtualDirents = Required<Omit<Dirents, "dirents">>;
 
 export interface DirElement extends Element
 {
+    fileSync(): ExistingDirents | VirtualDirents;
+
+    dirSync():ExistingDirents | VirtualDirents;
+
+    direntSync(): ExistingDirents | VirtualDirents;
+
     fileNamesSync(): ReadonlyArray<string>;
 
     dirNamesSync(): ReadonlyArray<string>;
