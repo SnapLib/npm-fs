07e60fea7289775e108ab6e2136f4140f915fcf2
Snap <XxSnapperGeexX@gmail.com>
2021-01-20 22:54:33 -0800

    If path points to pre-existing directory, add root contents to this virtual element

--- src/main/ts/lib/element/directory/VirtualDirElement.ts
+++ src/main/ts/lib/element/directory/VirtualDirElement.ts
@@ -1,15 +1,35 @@
 import AbstractDirElement from "./AbstractDirElement.js";
 import type { VirtualDirents } from "./DirElement.js";
+import fs from "fs";
 import path from "path";
 
 export class VirtualDirElement extends AbstractDirElement
 {
-    readonly #relativeFilePaths: Set<string> = new Set<string>();
-    readonly #relativeDirPaths: Set<string> = new Set<string>();
+    readonly #relativeFilePaths: Set<string>;
+    readonly #relativeDirPaths: Set<string>;
 
     public constructor(virtualDirPath:string, ...morePaths: ReadonlyArray<string>)
     {
         super(path.normalize([virtualDirPath].concat(morePaths).join(path.sep)), {exists: false});
+
+        if (fs.existsSync(this.path) && fs.lstatSync(this.path).isDirectory())
+        {
+            const dirents: ReadonlyArray<fs.Dirent> =
+                fs.readdirSync(this.path, {withFileTypes: true});
+
+            this.#relativeFilePaths =
+                new Set<string>(dirents.filter(dirent => dirent.isFile())
+                                       .map(fileDirent => path.join(this.path, fileDirent.name)));
+
+            this.#relativeDirPaths =
+                new Set<string>(dirents.filter(dirent => dirent.isDirectory())
+                                       .map(fileDirent => path.join(this.path, fileDirent.name)));
+        }
+        else
+        {
+            this.#relativeFilePaths = new Set<string>();
+            this.#relativeDirPaths = new Set<string>();
+        }
     }
 
     public fileSync(): VirtualDirents
