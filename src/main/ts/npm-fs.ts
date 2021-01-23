3b55f391541e7ba5154b53548f4966b75d75cb55
Snap <XxSnapperGeexX@gmail.com>
2021-01-22 18:55:49 -0800

    Remove section about assuming fie location

--- src/main/ts/npm-fs.ts
+++ src/main/ts/npm-fs.ts
@@ -6,11 +6,6 @@
  * declared in this npm package to be used by other npm packages that install it
  * as a dependency.
  *
- * @remarks
- * It is assumed that this package directory will be located in the root
- * `node_modules` directory and that this source file will be located in the
- * root of this package directory.
- *
  * @author Snap <XxSnapperGeexX@gmail.com>
  */
 export {ExistingFileElement} from "./lib/element/file/ExistingFileElement.js";
