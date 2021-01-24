a638b9931ff6fff5b9793c24050c1c3d6bdb8b84
Snap <XxSnapperGeexX@gmail.com>
2021-01-23 23:34:44 -0800

    Add doc comments for ExistingDirents and VirtualDirents types

--- src/main/ts/lib/element/directory/DirElement.ts
+++ src/main/ts/lib/element/directory/DirElement.ts
@@ -78,8 +78,21 @@ interface Dirents
     count: number;
 }
 
+/**
+ * Contains the type of properties the directory entries (dirents) of an
+ * existing directory element posses. The dirents of an existing directory
+ * element can be returned as absolute path strings, name strings, and as
+ * `Dirent` objects. The total number of directory entries can be returned as
+ * well.
+ */
 export type ExistingDirents = Required<Dirents>;
 
+/**
+ * Contains the type of properties the directory entries (dirents) of a virtual
+ * directory element posses. The dirents of a virtual directory element can be
+ * returned as absolute path strings and name strings. The total number of
+ * directory entries can be returned as well.
+ */
 export type VirtualDirents = Required<Omit<Dirents, "dirents">>;
 
 export {DirElement as default};
