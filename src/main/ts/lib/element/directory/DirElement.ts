32da1502cf01edb28501a7497aa664c5e3d70f06
Snap <XxSnapperGeexX@gmail.com>
2021-01-23 21:04:36 -0800

    Add doc comments

--- src/main/ts/lib/element/directory/DirElement.ts
+++ src/main/ts/lib/element/directory/DirElement.ts
@@ -3,11 +3,53 @@ import type {Dirent} from "fs";
 
 export interface DirElement extends Element
 {
-    fileSync(): ExistingDirents | VirtualDirents;
+    /**
+     * Returns in a synchronous manner this existing directory element's
+     * files filtered and formatted in various ways.
+     *
+     * @param options Whether to retrieve files recursively or not. If not set
+     *                to true, only directory entries contained in the root of
+     *                this existing directory element are returned.
+     *
+     * @returns The directory entries this existing directory element contains
+     *          filtered and formatted in various ways.
+     *
+     * @see dirSync
+     * @see direntSync
+     */
+    fileSync(options?: {recursive: boolean}): ExistingDirents | VirtualDirents;
 
-    dirSync():ExistingDirents | VirtualDirents;
+    /**
+     * Returns in a synchronous manner this existing directory element's
+     * directories filtered and formatted in various ways.
+     *
+     * @param options Whether to retrieve directories recursively or not. If not
+     *                set to true, only directory entries contained in the root
+     *                of this existing directory element are returned.
+     *
+     * @returns The directory entries this existing directory element contains
+     *          filtered and formatted in various ways.
+     *
+     * @see fileSync
+     * @see direntSync
+     */
+    dirSync(options?: {recursive: boolean}):ExistingDirents | VirtualDirents;
 
-    direntSync(): ExistingDirents | VirtualDirents;
+    /**
+     * Returns in a synchronous manner this existing directory element's
+     * directory entries filtered and formatted in various ways.
+     *
+     * @param options Whether to retrieve directory entries recursively or not.
+     *                If not set to true, only directory entries contained in
+     *                the root of this existing directory element are returned.
+     *
+     * @returns The directory entries this existing directory element contains
+     *          filtered and formatted in various ways.
+     *
+     * @see fileSync
+     * @see dirSync
+     */
+    direntSync(options?: {recursive: boolean}): ExistingDirents | VirtualDirents;
 
     contains(options?: {ignoreCase?: boolean, recursive?: boolean}): {
             file: (fileNameOrPath: string) => boolean,
