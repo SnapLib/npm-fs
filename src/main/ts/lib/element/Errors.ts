ee863d4fb66a5d8320c70dc96086d3633a5521dd
Snap <XxSnapperGeexX@gmail.com>
2021-02-03 22:05:35 -0800

    Create internal IllegalPathError class

--- src/main/ts/lib/element/Errors.ts
+++ src/main/ts/lib/element/Errors.ts
@@ -1,4 +1,33 @@
 /**
+ * Error thrown when an element is initialized with an illegal path string.
+ *
+ * @internal
+ * @abstract
+ */
+abstract class IllegalPathError extends Error
+{
+    public readonly path?: string;
+
+    /**
+    * Constructs an `IllegalPathError` with the optionally passed message string
+    * and path string that caused the error this error to be thrown.
+    *
+    * @param msg the message printed when this error is thrown
+    *
+    * @param path The path string that triggered this error to be
+    *             thrown
+     *
+     * @protected
+    */
+    protected constructor(msg?: string, path?: string)
+    {
+        super(msg);
+        this.path = path;
+    }
+
+}
+
+/**
  * Error thrown when an element is initialized with a mal formed path.
  *
  * This is only thrown if an element is passed an empty string argument during
@@ -6,14 +35,46 @@
  *
  * If stricter element path formats are enforced and trigger an error to be
  * thrown, the thrown error should extend from this error class.
+ *
+ * @sealed
  */
-export class MalFormedElementPathError extends Error
+export class MalFormedElementPathError extends IllegalPathError
 {
-    constructor(msg?: string)
-    {
-        super(msg);
+    /**
+     * @override
+     * @public
+     * @readonly
+     * @property
+     */
+    public readonly name: string = "MalFormedElementPathError";
 
-        // Maintains proper stack trace for where our error was thrown (only available on V8)
+    /**
+     * @public
+     * @readonly
+     * @property
+     */
+    public readonly regexPattern?: RegExp;
+
+    /**
+     * Constructs a `MalFormedElementPathError` with the optionally passed
+     * message string, mal formed path string, and regex pattern arguments.
+     *
+     * @param msg the message printed when this error is thrown
+     *
+     * @param path The path string that triggered this error to be
+     *             thrown
+     *
+     * @param regexPattern The regex pattern (if one was being used) that caused
+     *                     this error to be thrown
+     */
+    constructor(msg?: string, path?: string, regexPattern?: RegExp)
+    {
+        super(msg, path);
+
+        this.regexPattern = regexPattern;
+
+        // Maintains proper stack trace for where our error was thrown (only
+        // available on V8)
         if (Error.captureStackTrace)
         {
           Error.captureStackTrace(this, MalFormedElementPathError);
@@ -24,17 +85,44 @@ export class MalFormedElementPathError extends Error
 /**
  * Error thrown indicating that an existing element was passed a path that
  * doesn't exist (point to a pre-existing file or directory).
+ *
+ * @sealed
  */
-export class PathDoesNotExistError extends Error
+export class PathDoesNotExistError extends IllegalPathError
 {
-    constructor(msg?: string)
+    /**
+     * Constructs a `PathDoesNotExistError` with the optionally passed
+     * message string, mal formed path string, and regex pattern arguments.
+     *
+     * @param msg the message printed when this error is thrown
+     *
+     * @param path The path string that triggered this error to be
+     *             thrown
+     */
+    constructor(msg?: string, path?: string)
     {
-        super(msg);
+        super(msg, path);
 
-        // Maintains proper stack trace for where our error was thrown (only available on V8)
+        // Maintains proper stack trace for where our error was thrown (only
+        // available on V8)
         if (Error.captureStackTrace)
         {
           Error.captureStackTrace(this, PathDoesNotExistError);
+        }
+    }
+}
+
+export class IllegalPathType extends IllegalPathError
+{
+    constructor(msg?: string, path?: string)
+    {
+        super(msg, path);
+
+        // Maintains proper stack trace for where our error was thrown (only
+        // available on V8)
+        if (Error.captureStackTrace)
+        {
+          Error.captureStackTrace(this, IllegalPathType);
         }
     }
 }
