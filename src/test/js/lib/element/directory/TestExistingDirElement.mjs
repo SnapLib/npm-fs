600305b7424ff66bcb1c6333bf12c5c1ef50c324
Snap <XxSnapperGeexX@gmail.com>
2021-02-04 20:26:24 -0800

    Use absolute path to reference resources directory

--- src/test/js/lib/element/directory/TestExistingDirElement.mjs
+++ src/test/js/lib/element/directory/TestExistingDirElement.mjs
@@ -1,17 +1,20 @@
 import { ExistingDirElement } from "../../../../../../build/dist/npm-fs/lib/element/directory/ExistingDirElement.js";
 import chai from "chai";
 import { describe, it } from "mocha";
+import path from "path";
 
 const assert = chai.assert;
 
-const mockExistingDirPath = "../../../../resources/MockExistingDirectory";
+const mockExistingDirRelPath = path.resolve("../../../../resources/MockExistingDirectory");
+const mockExistingDirAbsPath = "/Users/Main/Projects/snaplib-npm-fs/src/test/resources/MockExistingDirectory";
 
 describe("MockExistingDirectory", function () {
-    describe(`creating existing dir element with: ${mockExistingDirPath}`, function ()
+    describe(`creating existing dir element with:\n"${mockExistingDirAbsPath}"`, function ()
     {
+        console.log(`\n\n${process.cwd()}\n\n`);
         it("should not throw when constructed", function ()
         {
-            assert.doesNotThrow(() => new ExistingDirElement(mockExistingDirPath));
+            assert.doesNotThrow(() => new ExistingDirElement(mockExistingDirAbsPath));
         });
     });
 });
