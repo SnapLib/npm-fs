52795b2d033b6d1aac65c74736fa63e0acdce7ee
Snap <XxSnapperGeexX@gmail.com>
2021-02-04 21:07:34 -0800

    Get cwd as extra test directory

--- src/test/js/lib/element/directory/TestExistingDirElement.mjs
+++ src/test/js/lib/element/directory/TestExistingDirElement.mjs
@@ -2,19 +2,36 @@ import { ExistingDirElement } from "../../../../../../build/dist/npm-fs/lib/elem
 import chai from "chai";
 import { describe, it } from "mocha";
 import path from "path";
+import url from "url";
 
 const assert = chai.assert;
+
+const fileUrlToPath = url.fileURLToPath;
+
+const cwd = path.dirname(fileUrlToPath(import.meta.url));
 
 const mockExistingDirRelPath = path.resolve("../../../../resources/MockExistingDirectory");
 const mockExistingDirAbsPath = "/Users/Main/Projects/snaplib-npm-fs/src/test/resources/MockExistingDirectory";
 
 describe("MockExistingDirectory", function () {
-    describe(`creating existing dir element with:\n"${mockExistingDirAbsPath}"`, function ()
+    describe("valid initialization", function ()
     {
-        console.log(`\n\n${process.cwd()}\n\n`);
-        it("should not throw when constructed", function ()
+        it(`new ExistingDirElement("${mockExistingDirAbsPath}") should not throw`, function ()
         {
             assert.doesNotThrow(() => new ExistingDirElement(mockExistingDirAbsPath));
+        });
+
+        it(`new ExistingDirElement("${cwd}") should not throw`, function ()
+        {
+            assert.doesNotThrow(() => new ExistingDirElement(cwd));
+        });
+    });
+
+    describe("invalid initialization", function ()
+    {
+        it(`new ExistingDirElement("") should not throw`, function ()
+        {
+            assert.doesNotThrow(() => new ExistingDirElement());
         });
     });
 });
