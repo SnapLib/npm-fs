a29901c0b3b328c424dd45a930ebf1e894442651
Snap <XxSnapperGeexX@gmail.com>
2021-02-10 20:48:50 -0800

    Add tests for scanDirTree() illegal arguments

--- src/test/ts/lib/util/TestDirTreeScanner.ts
+++ src/test/ts/lib/util/TestDirTreeScanner.ts
@@ -1,15 +1,36 @@
 import { scanDirTree, scanDirTreeUrl } from "../../../../main/ts/lib/util/dirTreeScanner";
 import { assert } from "chai";
 import { suite, test } from "mocha";
-import { basename, dirname } from "path";
+import { basename, dirname, join, resolve } from "path";
+
+const pkgRootPath = resolve(join(__dirname, "../../../../../"));
 
 suite("TestDirTreeScanner", function TestDirTreeScanner()
 {
+    suite("Test scan dir for illegal argument errors", function TestScanDirTreeError()
+    {
+        test(`scanDirTree("").for.file("package.json") throws`, function ()
+        {
+            assert.throws(() => scanDirTree("").for.file("package.json"));
+        });
+
+        test(`scanDirTree(__filename).for.directory("package.json") throws`, function ()
+        {
+            assert.throws(() => scanDirTree(__filename).for.directory("package.json"));
+        });
+
+    });
+
     suite("scanDirTree(<directoryname>).for.file(<filename>)", function TestScanDirTreeForFile()
     {
         test(`scanDirTree("${__dirname}").for.file("${basename(__filename)}") === "${__dirname}"`, function ()
         {
             assert.strictEqual(scanDirTree(__dirname).for.file(basename(__filename)), __dirname);
+        });
+
+        test(`scanDirTree("${__dirname}").for.file("package.json") === "${pkgRootPath}"`, function ()
+        {
+            assert.strictEqual(scanDirTree(__dirname).for.file("package.json"), pkgRootPath);
         });
     });
 
