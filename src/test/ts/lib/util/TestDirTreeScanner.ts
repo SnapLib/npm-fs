a41407fee1469d4c2b930410be0eca79a92f2742
Snap <XxSnapperGeexX@gmail.com>
2021-02-10 20:21:53 -0800

    Add test for using __dirname name as search query

--- src/test/ts/lib/util/TestDirTreeScanner.ts
+++ src/test/ts/lib/util/TestDirTreeScanner.ts
@@ -1,15 +1,23 @@
 import { scanDirTree, scanDirTreeUrl } from "../../../../main/ts/lib/util/dirTreeScanner";
 import { assert } from "chai";
 import { suite, test } from "mocha";
-import { basename } from "path";
+import { basename, dirname } from "path";
 
 suite("TestDirTreeScanner", function TestDirTreeScanner()
 {
-    suite("scanDirTree(__dirname).for.file(basename(__filename) === __dirname)", function TestElementType()
+    suite("scanDirTree(<directoryname>).for.file(<filename>)", function TestScanDirTreeForFile()
     {
         test(`scanDirTree("${__dirname}").for.file("${basename(__filename)}") === "${__dirname}"`, function ()
         {
             assert.strictEqual(scanDirTree(__dirname).for.file(basename(__filename)), __dirname);
+        });
+    });
+
+    suite("scanDirTree(<directoryname>).for.directory(<directoryname>)", function TestScanDirTreeForDirectory()
+    {
+        test(`scanDirTree("${__dirname}").for.directory("${basename(__dirname)}") === "${dirname(__dirname)}"`, function ()
+        {
+            assert.strictEqual(scanDirTree(__dirname).for.directory(basename(__dirname)), dirname(__dirname));
         });
     });
 });
