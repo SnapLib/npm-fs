7e5a237b52f96c698591e9beb93f0d48c38422ef
Snap <XxSnapperGeexX@gmail.com>
2021-02-10 20:04:07 -0800

    Name global suite function TestDirTreeScanner

---
+++ src/test/ts/lib/util/TestDirTreeScanner.ts
@@ -0,0 +1,17 @@
+import {scanDirTree, scanDirTreeUrl} from "../../../../main/ts/lib/util/dirTreeScanner";
+import { assert } from "chai";
+import { before, suite, test } from "mocha";
+
+suite("", function TestDirTreeScanner()
+{
+    before("Test object instantiation first", function TestInitializers()
+    {
+    });
+
+    suite("#elementType", function TestElementType()
+    {
+        test("", function ()
+        {
+        });
+    });
+});
