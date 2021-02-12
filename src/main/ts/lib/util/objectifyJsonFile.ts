7fcd29d490016b86388b65821d673542d24c8db9
Snap <XxSnapperGeexX@gmail.com>
2021-02-11 23:30:35 -0800

    Import and use type TranformObjectReturnType for function return type

--- src/main/ts/lib/util/objectifyJsonFile.ts
+++ src/main/ts/lib/util/objectifyJsonFile.ts
@@ -1,66 +1,23 @@
+import type { TransformObjectReturnType } from "./transformObject";
+import { transformObject } from "./transformObject";
 import fs from "fs";
 import ReadOnlyDict = NodeJS.ReadOnlyDict;
 
-export const transform = (obj: ReadOnlyDict<unknown>, options?: {keysToOmit?: ReadonlyArray<string>, keysToInclude?: ReadonlyArray<string>}, assignProperties?: ReadOnlyDict<unknown>): ReadOnlyDict<unknown> =>
-{
-    // Create new JS object with specified keys omitted or included
-    const newJsObj: ReadOnlyDict<unknown> =
-        Object.assign(
-            Object.fromEntries(Object.entries(obj)
-                                     .filter(pkgJsonEntry =>
-                                                 // If specified keys to include, only include those keys
-                                                 options?.keysToInclude?.includes(pkgJsonEntry[0])
-                                                 // If specified keys to exclude, include only keys that don't match those keys
-                                                 ?? ! options?.keysToOmit?.includes(pkgJsonEntry[0])
-                                                 // If no keys specified to include or exclude, include all keys
-                                                 ?? true)),
-            assignProperties ?? {});
-
-    // Create array of keys that are not present in the newly created JS object,
-    // but are present in the original JS object
-    const keysOmittedFromOriginalJSObj: ReadonlyArray<string> =
-        Object.keys(obj).filter(key => ! (key in newJsObj));
-
-    const keysIncludedFromOriginalJSObj: ReadonlyArray<string> =
-            Object.keys(obj).filter(key => ! keysOmittedFromOriginalJSObj.includes(key));
-
-    const keysUpdatedFromOriginalJSObj: ReadonlyArray<string> =
-        Object.keys(newJsObj).filter(key => (key in obj) && obj[key] !== newJsObj[key]);
-
-    // If there is a difference in keys between original and new JS object,
-    // print which keys are omitted and which keys are retained to console
-    if (keysOmittedFromOriginalJSObj.length !== 0)
-    {
-        console.log(`omitted keys from original package.json file:\n["${keysOmittedFromOriginalJSObj.join('", "')}"]\n`);
-    }
-
-    if (keysIncludedFromOriginalJSObj.length !== 0)
-    {
-        console.log(`retained keys from original package.json file:\n["${keysIncludedFromOriginalJSObj.join('", "')}"]\n`);
-    }
-
-    if (keysUpdatedFromOriginalJSObj.length !== 0)
-    {
-        const updatedKeysArrayShowingOldNewValues =
-            keysUpdatedFromOriginalJSObj.map(updatedKey => `${updatedKey}: "${obj[updatedKey]}" -> "${newJsObj[updatedKey]}"`);
-
-        console.log(`updated key values from original package.json file:\n{${updatedKeysArrayShowingOldNewValues.join(",\n ")}}\n`);
-    }
-
-    // New JS object with specified keys omitted, included, or updated
-    return newJsObj;
-};
-
-export const objectifyJsonString = (jsonString: string, options?: {keysToOmit?: ReadonlyArray<string>, keysToInclude?: ReadonlyArray<string>}, assignProperties?: ReadOnlyDict<unknown>): ReadOnlyDict<unknown> =>
+export const objectifyJsonString = (jsonString: string, options?: {keysToOmit?: ReadonlyArray<string>, keysToInclude?: ReadonlyArray<string>}, assignProperties?: ReadOnlyDict<unknown>): TransformObjectReturnType<unknown> =>
 {
     try
     {
         // New JS object with specified keys omitted, included, or updated
-        return transform(JSON.parse(jsonString), options, assignProperties);
+        return transformObject<unknown>(JSON.parse(jsonString), options, assignProperties);
     }
     catch (err)
     {
-        throw new Error(`error parsing package.json at "${jsonString}"`);
+        throw {
+            name: "ObjectTransformationError",
+            message: `Error transforming object: ${jsonString}`,
+            stack: err.stack
+
+        };
     }
 };
 
