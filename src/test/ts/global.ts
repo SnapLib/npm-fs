585c67d89331626fff6cd95a9e7ff633db8afa16
Snap <XxSnapperGeexX@gmail.com>
2021-02-12 21:24:39 -0800

    Init

---
+++ src/test/ts/global.ts
@@ -0,0 +1,30 @@
+import { join, resolve } from "path";
+
+export const TEST_RESOURCES = resolve(join(__dirname, "..", "resources"));
+export const PROJECT_PKG_ROOT = resolve(join(__dirname, "..", "..", ".."));
+
+export const project =
+{
+    root:
+    {
+        dir: join(PROJECT_PKG_ROOT),
+        packageJson: join(PROJECT_PKG_ROOT, "package.json"),
+        tsconfig: join(PROJECT_PKG_ROOT, "tsconfig.json")
+    },
+
+    src:
+    {
+        dir: join(PROJECT_PKG_ROOT, "src", "main", "ts"),
+        tsconfig: join(PROJECT_PKG_ROOT, "src", "main", "ts", "tsconfig.src.json")
+    },
+
+    test:
+    {
+        dir: __dirname,
+        resources: TEST_RESOURCES
+    }
+};
+
+console.log(project.test.resources);
+
+export {project as default};
