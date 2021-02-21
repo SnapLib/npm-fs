c87ed835bdfe5f1dd4f7541a6816cb8b6656a2fc
Snap <XxSnapperGeexX@gmail.com>
2021-02-21 11:01:09 -0800

    Switch test file extensions from .ts to .spect.ts

--- .root/.mocharc.js
+++ .root/.mocharc.js
@@ -4,10 +4,10 @@ const join = require("path").join
 
 module.exports =
 {
-    extension: ["ts"],
+    extension: ["spec.ts"],
     ignore: [join("src", "test", "ts", "globals.ts")],
     recursive: true,
     require: [join("ts-node", "register")],
-    spec: join("src", "test", "ts", "**", "*.ts"),
+    spec: join("src", "test", "ts", "**", "*.spec.ts"),
     ui: "tdd"
 }
