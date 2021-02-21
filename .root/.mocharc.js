6edac592c02db5219fb926a9caebd1589468355f
Snap <XxSnapperGeexX@gmail.com>
2021-02-21 10:57:23 -0800

    Move mocharc.js into .root directory and add spec property

--- .root/.mocharc.js
+++ .root/.mocharc.js
@@ -8,5 +8,6 @@ module.exports =
     ignore: [join("src", "test", "ts", "globals.ts")],
     recursive: true,
     require: [join("ts-node", "register")],
+    spec: join("src", "test", "ts", "**", "*.ts"),
     ui: "tdd"
 }
